ed747cf8fd765643af29223ba9d1b23468fe3873
aadityaraj25 <aditya.23b0121263@abes.ac.in>
2026-07-22 14:15:46 +0000

    added comment button

--- Frontend/src/components/TopNav.jsx
+++ Frontend/src/components/TopNav.jsx
@@ -33,6 +33,11 @@ export default function TopNav() {
             <path d="M13.7 21a2 2 0 01-3.4 0" />
           </svg>
         </button>
+        <button className="icon-btn" aria-label="Chat">
+          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+            <path d="M21 15a2 2 0 01-2 2H7l-4 4V5a2 2 0 012-2h14a2 2 0 012 2z" />
+          </svg>
+        </button>
         <div className="avatar av-a">DR</div>
       </div>
     </header>
